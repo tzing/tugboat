606ad4b7d6a50c1c8ac0c67e79a3f3fc1b543a66
Tzu-Ting <tzingshih@gmail.com>
2024-11-10 15:44:33 +0800

    add: workflow

--- tugboat/schemas/workflow.py
+++ tugboat/schemas/workflow.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Literal
+from typing import Any, Literal
 
 from pydantic import BaseModel, ConfigDict
 
@@ -21,11 +21,57 @@ class WorkflowTemplate(Workflow):
 
 
 class Spec(BaseModel):
+    """
+    `WorkflowSpec`_ is the specification of a Workflow.
+
+    .. _WorkflowSpec: https://argo-workflows.readthedocs.io/en/latest/fields/#workflowspec
+    """
+
+    model_config = ConfigDict(extra="forbid")
+
     arguments: Arguments | None = None
     entrypoint: str | None = None
-    templateDefaults: Template | None = None
     templates: list[Template] | None = None
     workflowTemplateRef: WorkflowTemplateRef | None = None
+
+    # acknowledged fields
+    activeDeadlineSeconds: int | None = None
+    affinity: Any | None = None
+    archiveLogs: bool | None = None
+    artifactGC: Any | None = None
+    artifactRepositoryRef: Any | None = None
+    automountServiceAccountToken: bool | None = None
+    dnsConfig: Any | None = None
+    dnsPolicy: str | None = None
+    executor: Any | None = None
+    hooks: Any | None = None
+    hostAliases: list[Any] | None = None
+    hostNetwork: bool | None = None
+    imagePullSecrets: list[Any] | None = None
+    metrics: Any | None = None
+    nodeSelector: dict[str, str] | None = None
+    onExit: str | None = None
+    parallelism: int | None = None
+    podDisruptionBudget: Any | None = None
+    podGC: Any | None = None
+    podMetadata: Any | None = None
+    podPriorityClassName: str | None = None
+    podSpecPatch: str | None = None
+    priority: int | None = None
+    retryStrategy: Any | None = None
+    schedulerName: str | None = None
+    securityContext: Any | None = None
+    serviceAccountName: str | None = None
+    shutdown: str | None = None
+    suspend: bool | None = None
+    synchronization: Any | None = None
+    templateDefaults: Template | None = None
+    tolerations: list[Any] | None = None
+    ttlStrategy: Any | None = None
+    volumeClaimGC: Any | None = None
+    volumeClaimTemplates: list[Any] | None = None
+    volumes: list[Any] | None = None
+    workflowMetadata: Any | None = None
 
 
 class WorkflowTemplateRef(BaseModel):
