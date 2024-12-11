06d1ada6cab97342ab90080055b52cd880d3843e
Tzu-Ting <tzingshih@gmail.com>
2024-12-12 00:08:44 +0800

    add: VirtualPath, cached_read

--- tests/console/test_utils.py
+++ tests/console/test_utils.py
@@ -1,7 +1,24 @@
-from tugboat.console.utils import format_loc
+import io
+import os
+
+from tugboat.console.utils import VirtualPath, format_loc
 
 
 class TestFormatLoc:
     def test(self):
         assert format_loc(["a", "b", "c"]) == ".a.b.c"
         assert format_loc(()) == "."
+
+
+class TestVirtualPath:
+    def test(self):
+        stream = io.StringIO("content")
+
+        path = VirtualPath("foo", stream)
+        assert isinstance(path, os.PathLike)
+
+        assert path.name == "foo"
+        with path.open() as fd:
+            assert fd.read() == "content"
+
+        assert repr(path) == "VirtualPath('foo')"
--- tugboat/console/utils.py
+++ tugboat/console/utils.py
@@ -1,10 +1,51 @@
 from __future__ import annotations
 
+import functools
+import io
 import typing
+from pathlib import Path
 
 if typing.TYPE_CHECKING:
     from collections.abc import Sequence
+    from typing import TextIO
 
 
 def format_loc(loc: Sequence[str | int]) -> str:
     return "." + ".".join(map(str, loc))
+
+
+class VirtualPath:
+    """
+    A :py:class:`pathlib.Path`-like object that wraps a stream and mimics the
+    behavior of a file on disk.
+
+    It could be used to represent files that are read from standard input or
+    generated on the fly.
+    """
+
+    def __init__(self, name: str, stream: TextIO):
+        self.name = name
+        self._stream = stream
+
+    def __fspath__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return f"VirtualPath({self.name!r})"
+
+    @functools.cache
+    def read_text(self) -> str:
+        return self._stream.read()
+
+    def open(self, mode: str = "r") -> TextIO:
+        if "r" not in mode:
+            return NotImplemented
+        return io.StringIO(self.read_text())
+
+
+@functools.lru_cache()
+def cached_read(path: Path) -> str:
+    """
+    Read the content of a file and cache it.
+    """
+    return path.read_text()
