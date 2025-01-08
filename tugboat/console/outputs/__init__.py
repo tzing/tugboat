e413bdb06461499b3b85b26682e3685b89f86764
Tzu-Ting <tzingshih@gmail.com>
2025-01-08 23:26:34 +0800

    add: get_output_builder

---
+++ tugboat/console/outputs/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import tugboat.settings
+from tugboat.console.outputs.base import OutputBuilder
+
+
+def get_output_builder() -> OutputBuilder:
+    match output_format := tugboat.settings.settings.output_format:
+        case "console":
+            from tugboat.console.outputs.console import ConsoleOutputBuilder
+
+            return ConsoleOutputBuilder()
+
+    raise RuntimeError(f"Unsupported output format: {output_format}")
