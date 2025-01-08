63d3971315e6c5aa9c8bf0aaf282b40e3df787bb
Tzu-Ting <tzingshih@gmail.com>
2025-01-08 22:19:18 +0800

    add: OutputBuilder

---
+++ tugboat/console/outputs/base.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import typing
+from abc import ABC, abstractmethod
+
+if typing.TYPE_CHECKING:
+    from collections.abc import Sequence
+    from pathlib import Path
+    from typing import TextIO
+
+    from tugboat.analyze import AugmentedDiagnosis
+
+
+class OutputBuilder(ABC):
+    """
+    Abstract base class for building diagnostic output.
+    """
+
+    @abstractmethod
+    def update(
+        self, *, path: Path, content: str, diagnoses: Sequence[AugmentedDiagnosis]
+    ) -> None:
+        """Write a diagnostic message for a given path."""
+
+    @abstractmethod
+    def dumps(self) -> str:
+        """Serialize the data to a str."""
+
+    def dump(self, stream: TextIO) -> None:
+        """Serialize the data to a stream."""
+        stream.write(self.dumps())
+        stream.write("\n")
