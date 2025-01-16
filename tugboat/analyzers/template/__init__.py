f37369b15df1910ea6b1b77f5beecf899fd49772
Tzu-Ting <tzingshih@gmail.com>
2025-01-16 22:12:37 +0800

    apply

--- tugboat/analyzers/template/__init__.py
+++ tugboat/analyzers/template/__init__.py
@@ -5,23 +5,23 @@ __all__ = ["check_input_artifact", "check_input_parameter"]
 import collections
 import typing
 
+from tugboat.analyzers.generic import check_model_fields_references
 from tugboat.analyzers.template.inputs import (
     check_input_artifact,
     check_input_parameter,
 )
 from tugboat.constraints import require_all, require_exactly_one
 from tugboat.core import hookimpl
-from tugboat.parsers import parse_template, report_syntax_errors
 from tugboat.references import get_template_context
 from tugboat.utils import prepend_loc
 
 if typing.TYPE_CHECKING:
     from collections.abc import Iterable
 
+    from pydantic import BaseModel
+
     from tugboat.schemas import Template, Workflow, WorkflowTemplate
     from tugboat.types import Diagnosis
-
-    type DocumentMap = dict[tuple[str | int, ...], str]
 
 
 @hookimpl
@@ -83,17 +83,7 @@ def check_field_references(
     Implemented:
 
     - `container`
-      - `args`
-      - `command`
-      - `image`
-      - `stdin`
-      - `workingDir`
-
     - `script`
-      - `command`
-      - `image`
-      - `source`
-      - `workingDir`
 
     Intended excluded:
 
@@ -101,46 +91,23 @@ def check_field_references(
     - `outputs` - handled in `check_output_parameters` and `check_output_artifacts` above
     - `steps` - handled separately in another hook
     """
-    # collect fields
-    fields: DocumentMap = {}
+    context = get_template_context(workflow, template)
 
+    models: dict[tuple[str, ...], BaseModel] = {}
     if template.container:
-        for i, command in enumerate(template.container.command or []):
-            fields["container", "command", i] = command
-        for i, arg in enumerate(template.container.args or []):
-            fields["container", "args", i] = arg
-
-        fields["container", "image"] = template.container.image
-        if template.container.workingDir:
-            fields["container", "workingDir"] = template.container.workingDir
-
+        models["container",] = template.container
     if template.script:
-        for i, command in enumerate(template.script.command or []):
-            fields["script", "command", i] = command
+        models["script",] = template.script
 
-        fields["script", "image"] = template.script.image
-        fields["script", "source"] = template.script.source
-        if template.script.workingDir:
-            fields["script", "workingDir"] = template.script.workingDir
-
-    if not fields:
-        return
-
-    # check each field for template syntax and references
-    ctx = get_template_context(workflow, template)
-
-    for loc, value in fields.items():
-        doc = parse_template(value)
-        yield from prepend_loc(loc, report_syntax_errors(doc))
-
-        for node, ref, closest in ctx.parameters.filter_unknown(doc.iter_references()):
-            yield {
-                "code": "VAR002",
-                "loc": loc,
-                "summary": "Invalid reference",
-                "msg": (
-                    f"""The parameter reference '{".".join(ref)}' used in template '{template.name}' is invalid."""
-                ),
-                "input": str(node),
-                "fix": node.format(closest),
-            }
+    for loc, model in models.items():
+        for diag in prepend_loc(
+            loc, check_model_fields_references(model, context.parameters)
+        ):
+            match diag["code"]:
+                case "VAR002":
+                    ctx = typing.cast(dict, diag.get("ctx"))
+                    ref = ".".join(ctx["ref"])
+                    diag["msg"] = (
+                        f"The parameter reference '{ref}' used in template '{template.name}' is invalid."
+                    )
+            yield diag
