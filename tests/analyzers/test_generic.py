144bad69432063e70cfa3ea642eee46fdb8edeb4
Tzu-Ting <tzingshih@gmail.com>
2025-01-16 20:57:44 +0800

    add: check_argo_variable_errors

--- tests/analyzers/test_generic.py
+++ tests/analyzers/test_generic.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
-from tugboat.analyzers.generic import report_duplicate_names
+from pydantic import BaseModel
+
+from tugboat.analyzers.generic import check_argo_variable_errors, report_duplicate_names
+from tugboat.references.context import ReferenceCollection
 from tugboat.schemas import Parameter
 
 
@@ -17,3 +20,34 @@ class TestReportDuplicateNames:
             Parameter(name="name-1"),
         ]
         assert list(report_duplicate_names(items)) == [(0, "name-1"), (2, "name-1")]
+
+
+class TestCheckArgoVariableErrors:
+
+    def test_pass(self):
+        class Model(BaseModel):
+            field1: str = "value"
+            field2: int = 1234
+
+        assert list(check_argo_variable_errors(Model(), ReferenceCollection())) == []
+
+    def test_picked(self):
+        class Nested(BaseModel):
+            field1: int = 1234
+            field2: str = "{{ invalid }}"
+
+        class Model(BaseModel):
+            array: list[Nested]
+
+        model = Model(array=[Nested()])
+
+        assert list(check_argo_variable_errors(model, ReferenceCollection())) == [
+            {
+                "code": "VAR002",
+                "loc": ("array", 0, "field2"),
+                "summary": "Invalid reference",
+                "msg": "The used reference 'invalid' is invalid.",
+                "input": "{{ invalid }}",
+                "fix": None,
+            }
+        ]
--- tugboat/analyzers/generic.py
+++ tugboat/analyzers/generic.py
@@ -3,12 +3,20 @@ from __future__ import annotations
 import collections
 import typing
 
+from pydantic import BaseModel
+
+from tugboat.parsers import parse_template, report_syntax_errors
+from tugboat.utils import prepend_loc
+
 if typing.TYPE_CHECKING:
     from collections.abc import Iterator, Sequence
 
+    from tugboat.references.context import ReferenceCollection
     from tugboat.schemas import Artifact, Parameter, Template
+    from tugboat.types import Diagnosis
 
     type NamedModel = Artifact | Parameter | Template
+    type DocumentMap = dict[tuple[str | int, ...], str]
 
 
 def report_duplicate_names(items: Sequence[NamedModel]) -> Iterator[tuple[int, str]]:
@@ -23,3 +31,55 @@ def report_duplicate_names(items: Sequence[NamedModel]) -> Iterator[tuple[int, s
         if len(indices) > 1:
             for idx in indices:
                 yield idx, name
+
+
+def check_argo_variable_errors(
+    model: BaseModel, references: ReferenceCollection
+) -> Iterator[Diagnosis]:
+    """
+    Check the fields of the given model for errors that are specific to Argo
+    workflows variables.
+
+    Parameters
+    ----------
+    model : BaseModel
+        The model to check. This function will check all fields of the model
+        that are of type str.
+    references : ReferenceCollection
+        The reference collection for the current context.
+
+    Yields
+    ------
+    """
+    # collect all the fields that are strings
+    sources: DocumentMap = {}
+
+    def _check(model: BaseModel, loc: tuple[str | int, ...] = ()):
+        for field, value in model:
+            if isinstance(value, str):
+                sources[*loc, field] = value
+            if isinstance(value, BaseModel):
+                _check(value, (*loc, field))
+            if isinstance(value, list):
+                for idx, item in enumerate(value):
+                    if isinstance(item, BaseModel):
+                        _check(item, (*loc, field, idx))
+
+    _check(model)
+
+    # check templates
+    for loc, value in sources.items():
+        doc = parse_template(value)
+        yield from prepend_loc(loc, report_syntax_errors(doc))
+
+        for node, ref in doc.iter_references():
+            if ref not in references:
+                closest = references.find_closest(ref)
+                yield {
+                    "code": "VAR002",
+                    "loc": loc,
+                    "summary": "Invalid reference",
+                    "msg": f"The used reference '{".".join(ref)}' is invalid.",
+                    "input": str(node),
+                    "fix": node.format(closest),
+                }
