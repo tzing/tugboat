32f4e65b924e632d50187bd0cdef765fe8db62af
Tzu-Ting <tzingshih@gmail.com>
2025-04-08 22:43:31 +0800

    rm: path pattern

--- tests/test_types.py
+++ tests/test_types.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 import pytest
 from dirty_equals import IsInstance
-from pydantic import BaseModel, TypeAdapter
+from pydantic import BaseModel
 
-from tugboat.types import GlobPath, PathLike, PathPattern
+from tugboat.types import GlobPath, PathLike
 
 
 class TestPathLike:
@@ -62,45 +62,3 @@ class TestGlobPath:
     def test_failed(self):
         with pytest.raises(ValueError, match="Pattern 'foo' is not a glob pattern"):
             GlobPath("foo")
-
-
-class TestPathPattern:
-
-    def test_pydantic(self):
-        ta = TypeAdapter(PathPattern)
-
-        obj = ta.validate_python("foo")
-        assert isinstance(obj, PathPattern)
-        assert obj.pattern == os.path.realpath("foo")
-
-        assert isinstance(ta.validate_python(obj), PathPattern)
-
-    def test_match_1(self):
-        pp = PathPattern("foo")
-        assert pp == "foo"
-        assert pp == Path("foo")
-        assert pp != "fooo"
-        assert pp == PathPattern("foo")
-        assert pp != PathPattern("foo/bar")
-
-    def test_match_2(self):
-        pp = PathPattern("**/*.yaml")
-        assert pp == "foo.yaml"
-        assert pp == Path("bar/foo.yaml")
-        assert pp != "foo.YAML"
-
-    def test_repr(self):
-        pp = PathPattern("foo")
-        assert isinstance(repr(pp), str)
-
-    def test_iglob_1(self, fixture_dir: Path):
-        sample_workflow = fixture_dir / "sample-workflow.yaml"
-
-        pattern = PathPattern(fixture_dir / "*.yaml")
-        assert sample_workflow in list(pattern.iglob())
-
-    def test_iglob_2(self):
-        this_dir = Path(__file__).parent
-
-        pattern = PathPattern(this_dir / "**" / "*.py")
-        assert Path(__file__) in list(pattern.iglob(recursive=True))
