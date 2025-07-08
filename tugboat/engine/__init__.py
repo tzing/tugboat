2c6e36047612334547d3a89baa64ae7719d9a965
tzing <tzingshih@gmail.com>
2025-07-08 23:09:55 +0800

    move: AugmentedDiagnosis

--- tugboat/engine/__init__.py
+++ tugboat/engine/__init__.py
@@ -1,3 +1,11 @@
 """
 As the engine hums, the boats remembers why it sails.
 """
+
+from __future__ import annotations
+
+__all__ = [
+    "AugmentedDiagnosis",
+]
+
+from tugboat.engine.types import AugmentedDiagnosis
--- tugboat/types.py
+++ tugboat/types.py
@@ -79,53 +79,6 @@ class Diagnosis(TypedDict):
     """Additional context."""
 
 
-class AugmentedDiagnosis(TypedDict):
-    """
-    The augmented diagnosis reported by the analyzer.
-
-    This type extends the :py:class:`Diagnosis` and adds additional fields
-    to provide more context about the diagnosis.
-    """
-
-    line: int
-    """
-    Line number of the issue occurrence in the source file.
-    Note that the line number is cumulative across all documents in the YAML file.
-    """
-
-    column: int
-    """
-    Column number of the issue occurrence in the source file.
-    """
-
-    type: Literal["error", "failure", "warning"]
-    """The type that describes the severity."""
-
-    code: str
-    """Diagnosis code."""
-
-    manifest: str | None
-    """The manifest name where the issue occurred."""
-
-    loc: tuple[str | int, ...]
-    """
-    The location of the issue occurrence within the manifest, specified in a
-    path-like format.
-    """
-
-    summary: str
-    """The summary."""
-
-    msg: str
-    """The detailed message."""
-
-    input: Any | None
-    """The input that caused the issue."""
-
-    fix: str | None
-    """The possible fix for the issue."""
-
-
 class PathLike:
     """
     A base class that implements the :py:class:`os.PathLike` interface and works
