d84d6a8394e9e84bfde500ed5326cb666a2e184f
tzing <tzingshih@gmail.com>
2025-10-27 22:51:43 +0800

    add: dag102, dag103

--- docs/rules/dag.rst
+++ docs/rules/dag.rst
@@ -37,3 +37,59 @@ Code ``DAG`` is used for errors related to the `DAG`_ in a `template`_.
                     parameters:
                       - name: message
                         value: "hello-2"
+
+.. rule:: DAG102 Duplicate input parameters
+
+   The task contains several input parameters (``<task>.arguments.parameters``) that share the same name,
+   which means the parameter was set multiple times.
+
+   .. code-block:: yaml
+      :emphasize-lines: 15,17
+
+      apiVersion: argoproj.io/v1alpha1
+      kind: Workflow
+      metadata:
+        name: test-
+      spec:
+        entrypoint: main
+        templates:
+          - name: main
+            dag:
+              tasks:
+                - name: hello
+                  template: print-message
+                  arguments:
+                    parameters:
+                      - name: message
+                        value: "hello"
+                      - name: message
+                        value: "world"
+
+.. rule:: DAG103 Duplicate input artifacts
+
+   The task includes several input artifacts (``<task>.arguments.artifacts``) that share the same name.
+   The artifact was set multiple times.
+
+   .. code-block:: yaml
+      :emphasize-lines: 15,18
+
+      apiVersion: argoproj.io/v1alpha1
+      kind: Workflow
+      metadata:
+        name: test-
+      spec:
+        entrypoint: main
+        templates:
+          - name: main
+            dag:
+              tasks:
+                - name: process-data
+                  template: data-processor
+                  arguments:
+                    artifacts:
+                      - name: input-data
+                        raw:
+                          data: "data-1"
+                      - name: input-data
+                        raw:
+                          data: "data-2"
--- tests/analyzers/test_dag.py
+++ tests/analyzers/test_dag.py
@@ -32,3 +32,68 @@ def test_analyze_task(diagnoses_logger):
     loc = ("spec", "templates", 0, "dag", "tasks", 0)
     assert IsPartialModel({"code": "M201", "loc": (*loc, "depends")}) in diagnoses
     assert IsPartialModel({"code": "M201", "loc": (*loc, "dependencies")}) in diagnoses
+
+
+def test_check_argument_parameters(diagnoses_logger):
+    diagnoses = analyze_yaml_stream(MANIFEST_INVALID_INPUT_PARAMETERS)
+    diagnoses_logger(diagnoses)
+
+    loc_prefix = ("spec", "templates", 0, "dag", "tasks", 0, "arguments", "parameters")
+
+    # DAG102: Duplicate parameter name
+    assert IsPartialModel(code="DAG102", loc=(*loc_prefix, 0, "name")) in diagnoses
+    assert IsPartialModel(code="DAG102", loc=(*loc_prefix, 1, "name")) in diagnoses
+
+
+MANIFEST_INVALID_INPUT_PARAMETERS = """
+apiVersion: argoproj.io/v1alpha1
+kind: Workflow
+metadata:
+  generateName: test-
+spec:
+  entrypoint: main
+  templates:
+    - name: main
+      dag:
+        tasks:
+          - name: step1
+            arguments:
+              parameters:
+                - name: param1
+                  value: "value1"
+                - name: param1
+                  value: "value2"
+"""
+
+
+def test_check_argument_artifacts(diagnoses_logger):
+    diagnoses = analyze_yaml_stream(MANIFEST_INVALID_INPUT_ARTIFACTS)
+    diagnoses_logger(diagnoses)
+
+    loc_prefix = ("spec", "templates", 0, "dag", "tasks", 0, "arguments", "artifacts")
+
+    # DAG103: Duplicate artifact name
+    assert IsPartialModel(code="DAG103", loc=(*loc_prefix, 0, "name")) in diagnoses
+    assert IsPartialModel(code="DAG103", loc=(*loc_prefix, 1, "name")) in diagnoses
+
+
+MANIFEST_INVALID_INPUT_ARTIFACTS = """
+apiVersion: argoproj.io/v1alpha1
+kind: Workflow
+metadata:
+  generateName: test-
+spec:
+  entrypoint: main
+  templates:
+    - name: main
+      dag:
+        tasks:
+          - name: step1
+            arguments:
+              artifacts:
+                - name: artifact1
+                  from: "{{workflow.invalid}}"
+                - name: artifact1
+                  raw:
+                    data: "some data"
+"""
