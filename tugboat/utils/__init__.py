bac85e7481c81cf022cd52e53e10f8faac10f926
tzing <tzingshih@gmail.com>
2025-12-03 23:10:48 +0800

    rm: remove field checker functions

--- tugboat/utils/__init__.py
+++ tugboat/utils/__init__.py
@@ -1,19 +1,9 @@
 __all__ = [
-    "check_model_fields_references",
-    "check_value_references",
     "find_duplicate_names",
     "join_with_and",
     "join_with_or",
     "prepend_loc",
 ]
 
-from tugboat.utils.humanize import (
-    join_with_and,
-    join_with_or,
-)
-from tugboat.utils.operator import (
-    check_model_fields_references,
-    check_value_references,
-    find_duplicate_names,
-    prepend_loc,
-)
+from tugboat.utils.humanize import join_with_and, join_with_or
+from tugboat.utils.operator import find_duplicate_names, prepend_loc
