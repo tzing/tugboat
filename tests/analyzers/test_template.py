7e797989ffe95732eba7a8e44f4de873c8aec069
tzing <tzingshih@gmail.com>
2025-12-03 19:10:01 +0800

    chore: apply to tempalte analyzer

--- tests/analyzers/test_template.py
+++ tests/analyzers/test_template.py
@@ -32,10 +32,10 @@ class TestGeneralRules:
         assert (
             IsPartialModel(
                 {
-                    "code": "VAR002",
+                    "code": "VAR201",
                     "loc": ("spec", "templates", 0, "container", "args", 1),
                     "msg": "The parameter reference 'inputs.parameters.command' used in template 'container-template' is invalid.",
-                    "fix": "{{ inputs.parameters.cmd }}",
+                    "fix": "inputs.parameters.cmd",
                 }
             )
             in diagnoses
@@ -43,10 +43,10 @@ class TestGeneralRules:
         assert (
             IsPartialModel(
                 {
-                    "code": "VAR002",
+                    "code": "VAR201",
                     "loc": ("spec", "templates", 1, "script", "source"),
                     "msg": "The parameter reference 'inputs.artifacts.data' used in template 'script-template' is invalid.",
-                    "fix": "{{ inputs.artifacts.data.path }}",
+                    "fix": "inputs.artifacts.data.path",
                 }
             )
             in diagnoses
@@ -221,13 +221,12 @@ def test_check_input_parameters_1(diagnoses_logger):
     assert IsPartialModel(code="TPL102", loc=(*loc, 0, "name")) in diagnoses
     assert IsPartialModel(code="TPL102", loc=(*loc, 1, "name")) in diagnoses
     assert IsPartialModel(code="M102", loc=(*loc, 0, "valueFrom", "path")) in diagnoses
-    assert IsPartialModel(code="VAR001", loc=(*loc, 1, "value")) in diagnoses
+    assert IsPartialModel(code="VAR101", loc=(*loc, 1, "value")) in diagnoses
     assert (
         IsPartialModel(
             code="TPL201",
             loc=(*loc, 2, "value"),
             msg="The parameter reference 'workflow.invalid' used in parameter 'message-3' is invalid.",
-            input="{{ workflow.invalid}}",
         )
         in diagnoses
     )
@@ -307,7 +306,6 @@ def test_check_input_artifacts(diagnoses_logger):
             msg=HasSubstring(
                 "The parameter reference 'workflow.namee' used in artifact 'data' is invalid."
             ),
-            fix="{{ workflow.name }}",
         )
         in diagnoses
     )
@@ -344,7 +342,7 @@ def test_check_output_parameters(diagnoses_logger):
                   - name: message # TPL104
                   - name: message # TPL104
                     valueFrom:
-                      parameter: "{{ workflow.invalid}}" # VAR002
+                      parameter: "{{ workflow.invalid}}" # VAR201
 
             - name: main
               suspend: {}
@@ -364,7 +362,7 @@ def test_check_output_parameters(diagnoses_logger):
 
     assert IsPartialModel(code="M101", loc=(*loc, 0, "valueFrom")) in diagnoses
     assert (
-        IsPartialModel(code="VAR002", loc=(*loc, 1, "valueFrom", "parameter"))
+        IsPartialModel(code="VAR201", loc=(*loc, 1, "valueFrom", "parameter"))
         in diagnoses
     )
 
@@ -401,7 +399,7 @@ def test_check_output_artifacts(diagnoses_logger):
 
     assert IsPartialModel(code="M101", loc=(*loc, 0, "archive")) in diagnoses
     assert IsPartialModel(code="M102", loc=(*loc, 0, "path")) in diagnoses
-    assert IsPartialModel(code="VAR002", loc=(*loc, 1, "from")) in diagnoses
+    assert IsPartialModel(code="VAR202", loc=(*loc, 1, "from")) in diagnoses
 
 
 def test_check_metrics(diagnoses_logger):
@@ -431,7 +429,7 @@ def test_check_metrics(diagnoses_logger):
                     - key: invalid-label
                       value: ""
                   counter:
-                    value: "{{ outputs.parameters.no-param }}" # VAR002
+                    value: "{{ outputs.parameters.no-param }}" # VAR201
         """
     )
     diagnoses_logger(diagnoses)
